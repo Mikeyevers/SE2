2c6baaa929b75c4548ae6ce1cea4910370abe194
Mike Evers <mike_evers1997@hotmail.com>
2015-06-11 18:26:11 +0200

    disabled logging in after already logged in

--- IndividueleOpdracht/Login.aspx.cs
+++ IndividueleOpdracht/Login.aspx.cs
@@ -13,6 +13,14 @@ namespace IndividueleOpdracht
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Er dient gecontroleerd worden of er is ingelogd.
+            // Indien dit het geval is dan dient er naar de (door de klant) ingestelde "pageAfterLogin" (Web.config)
+            // genavigeerd te worden.
+            if (Session.Count != 0 || Session["UserAuthentication"] != null)
+            {
+                NavigateAfterLogin(this.Response);
+            }
+
             // Administration is de class die o.a. de database connectie bevat,         
             administration = new Administration();
         }
@@ -21,21 +29,25 @@ namespace IndividueleOpdracht
 
         public void LoginBtn_Click(object sender, EventArgs e)
         {
-            if (administration.Inloggen(inputEmail.Text, inputPassword.Text))
+            if (Page.IsValid)
             {
-                // Session aanmaken voor de ingelogde gebruiker.
-                Session["UserAuthentication"] = inputEmail.Text;
+                if (administration.Inloggen(inputEmail.Text, inputPassword.Text))
+                {
+                    // Session aanmaken voor de ingelogde gebruiker.
+                    Session["UserAuthentication"] = inputEmail.Text;
 
-                // Ik heb een methode geschreven die na het inloggen ervoor zorgt dat er naar een bepaalde pagina wordt genavigeerd.
-                // Deze pagina is flexibel en kan door de klant in Web.config worden aangepast. 
-                NavigateAfterLogin(this.Response);
+                    // Ik heb een methode geschreven die na het inloggen ervoor zorgt dat er naar een bepaalde pagina wordt genavigeerd.
+                    // Deze pagina is flexibel en kan door de klant in Web.config worden aangepast. 
+                    NavigateAfterLogin(this.Response);
+                }
+                else
+                {
+                    // Laat gebruiker zien dat inloggen is mislukt.
+                    LoginFailureText.Text = "<span class=\"text-warning\">Inloggen mislukt. Controleer je wachtwoord en probeer het opnieuw.</span>";
+                    LoginFailureText.Visible = true;
+                }
             }
-            else
-            {
-                // Laat gebruiker zien dat inloggen is mislukt.
-                LoginFailureText.Text = "<span class=\"text-warning\">Inloggen mislukt. Controleer je wachtwoord en probeer het opnieuw.</span>";
-                LoginFailureText.Visible = true;
-            }
+            
         }
             
            
--- IndividueleOpdracht/Masterpage.Master.designer.cs
+++ IndividueleOpdracht/Masterpage.Master.designer.cs
@@ -22,15 +22,6 @@ namespace IndividueleOpdracht {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// ScriptManager1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.ScriptManager ScriptManager1;
-        
-        /// <summary>
         /// btn_uitloggen control.
         /// </summary>
         /// <remarks>
